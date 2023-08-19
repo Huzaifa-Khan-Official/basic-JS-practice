a64a5c5df7c4449a4d4c162d568799667e7e3f6a
Huzaifa Khan <122217933+Huzaifa-Khan0@users.noreply.github.com>
2023-08-19 11:37:11 +0500

    first

---
+++ Completed Assignments/chapter1/app.js
@@ -0,0 +1,12 @@
+// alert("Hello! How are you?"); 1
+
+
+// alert("Erroe! Please enter a valid password."); 2
+
+// alert("Welcome to JS Land.... \nHappy Coding!"); 3 
+
+// alert("Welcome to JS Land...");
+// alert("Happy Coding! \nPrevent this page from creating additional dialogs."); 4
+
+alert("Hello... I can run JS through my web browser's console"); 5
+
---
+++ Completed Assignments/chapter2/app.js
@@ -0,0 +1,29 @@
+// var userName; (1)
+
+// var myName = "Muhammad Huzaifa"; (2)
+
+// var message = "Hello World" 
+// alert(message); (3)
+
+// var stu_Name = "Muhammad Huzaifa";
+// var stu_Age = "19 Years old";
+// var stu_Course = "Web And Mobile App Development";
+
+// alert(stu_Name);
+// alert(stu_Age);
+// alert(stu_Course); (4)
+
+// var food = "PIZZA";
+// alert("PIZZA\nPIZZ\nPIZ\nPI\nP"); (5)
+
+// var email = "m.huzaifakhan2004@gmail.com";
+// alert("My email address is " + email); (6)
+
+// var book = "A smarter way to learn JavaScript";
+// alert("I'm trying to learn form the Book " + book); (7)
+
+// document.write("Yeh! I can write HTML content through JavaScript"); (8)
+
+var diagram = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(diagram);
+// (9)
