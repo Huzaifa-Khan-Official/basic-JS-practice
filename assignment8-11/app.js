6b7d0f36a90d167f6d25e3e049ab746745a50e38
Huzaifa Khan <122217933+Huzaifa-Khan0@users.noreply.github.com>
2023-09-11 17:21:54 +0500

    chapter 8 - 11

---
+++ assignment8-11/app.js
@@ -0,0 +1,131 @@
+// 1st question
+// var name = prompt("Enter your name: ");
+// alert("Hi " + name);
+
+// 2nd question
+// var number = +prompt("Enter a number for multiplication: ");
+// if(number == " "){
+//     for(var i = 1; i <= 10; i++){
+        
+//         result = i * 5;
+//         document.write("5 X "+ i + " " + "= " + result, "<br>");
+//     }
+// } else {
+//     for(var i = 1; i <= 10; i++){
+//         result = i * number;
+//         document.write(number + " X "+ i + " " + "= " + result, "<br>");
+//     }
+// }
+
+
+// 3rd question
+// var city = prompt("Ente city name: ");
+// if(city == "karachi") {
+//     alert("Welcome to the city of lights.");
+// } else {
+//     alert("Welcome to " + city);
+// };
+
+
+// 4th question
+
+// var gender = prompt("Enter your gender: ");
+// if(gender == "male"){
+//     alert("Welcome Sir!");
+// } else if (gender == "female") {
+//     alert("Welcome Ma'am!");
+// } else { 
+//     alert("Welcome!");
+// };
+
+
+// 5th question
+
+// var roadSignal =  prompt("Enter road signal: ");
+// if(roadSignal == "red"){
+//     alert("Vehicles must stop!");
+// } else if (roadSignal == "yellow") {
+//     alert("Vehicles should get ready to move!");
+// } else if (roadSignal == "green") {
+//     alert("Vehicles can move now!");
+// } else {
+//     alert("Wrong road traffic signal!");
+// };
+
+
+// 6th question
+
+// var currAge = prompt("Enter your current age: ");
+// var maxAge = prompt("Enter your maximum age: ");
+// if (currAge <= maxAge){
+//     alert("“You are welcome!")
+// } else {
+//     alert("You are not allowed!")
+// }
+
+
+// 7th question
+
+// var fuel = prompt("Enter remaining fuel: (In liters)");
+// if (fuel < 0.25) {
+//     alert("“Please refill the fuel in your car!");
+// } else {
+//     alert("“You don't need to refill the fuel in your car!");
+// };
+
+
+// 8th question
+
+// var a = 4;
+// if (++a === 5){
+//  alert("given condition for variable a is true");
+// }
+// alert displays
+
+// var b = 82;
+// if (b++ === 83){
+//  alert("given condition for variable b is true");
+// };
+// alert not displays 
+
+
+// var c = 12;
+// if (c++ === 13){
+//  alert("condition 1 is true");
+// }
+// alert not displays
+// if (c === 13){
+//  alert("condition 2 is true");
+// }
+// alert displays
+
+// if (++c < 14){
+//  alert("condition 3 is true");
+// }
+// alert not displays
+// if(c === 14){
+//  alert("condition 4 is true");
+// }
+// alert displays
+
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+//  alert("The cost equals");
+// };
+// alert displays
+
+// if (true){
+//     alert("True");
+// }
+// alert displays
+// if (false){
+//     alert("False");
+// }
+// alert not displays
+
+// if("car" < "cat"){
+//     alert("car is smaller than cat");
+// };
+// alert displays
