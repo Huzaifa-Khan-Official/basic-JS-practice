10ddfeca05ed46d3ecfd8c4f11f6a323397f9bc8
Huzaifa Khan <122217933+Huzaifa-Khan-Official@users.noreply.github.com>
2023-10-04 11:36:20 -0700

    starting26-34

---
+++ 26-34 Assignment/app.js
@@ -0,0 +1,103 @@
+// ************* Question : 1 *****************
+
+// let num = prompt("Enter a positive number");
+// let roundoffValue = Math.round(num);
+// let floorValue = Math.floor(num);
+// let ceilValue = Math.ceil(num)
+// console.log(num, roundoffValue, floorValue, ceilValue);
+
+// document.write(`
+// <p>Number: ${num}</p>
+// <p>Round Off Value: ${roundoffValue}</p>
+// <p>Floor Value: ${floorValue}</p>
+// <p>Ceil Value: ${ceilValue}</p>
+// `)
+
+
+// ************* Question : 2 *****************
+
+// let num = prompt("Enter a Negative number:");
+// let roundoffValue = Math.round(num);
+// let floorValue = Math.floor(num);
+// let ceilValue = Math.ceil(num)
+// console.log(num, roundoffValue, floorValue, ceilValue);
+
+// document.write(`
+// <p>Number: ${num}</p>
+// <p>Round Off Value: ${roundoffValue}</p>
+// <p>Floor Value: ${floorValue}</p>
+// <p>Ceil Value: ${ceilValue}</p>
+// `);
+
+
+// ************* Question : 3 *****************
+
+// let num = prompt("Enter a Positive Floating number:");
+// let roundoffValue = Math.round(num);
+// let floorValue = Math.floor(num);
+// let ceilValue = Math.ceil(num)
+// console.log(num, roundoffValue, floorValue, ceilValue);
+
+// document.write(`
+// <p>Number: ${num}</p>
+// <p>Round Off Value: ${roundoffValue}</p>
+// <p>Floor Value: ${floorValue}</p>
+// <p>Ceil Value: ${ceilValue}</p>
+// `);
+
+
+// ************* Question : 4 *****************
+
+// let num = prompt("Enter a Positive Floating number:");
+// let roundoffValue = Math.round(num);
+// let floorValue = Math.floor(num);
+// let ceilValue = Math.ceil(num)
+// console.log(num, roundoffValue, floorValue, ceilValue);
+
+// document.write(`
+// <p>Number: ${num}</p>
+// <p>Round Off Value: ${roundoffValue}</p>
+// <p>Floor Value: ${floorValue}</p>
+// <p>Ceil Value: ${ceilValue}</p>
+// `);
+
+
+// ************* Question : 5 *****************
+
+// let num = prompt("Enter a value to find its absolute: ");
+// let absoluteValue = Math.abs(num);
+// document.write(`
+// <p>The absolute value of ${num} is ${absoluteValue}.</p>
+// `);
+
+
+// ************* Question : 6 *****************
+
+// let diceValue = Math.floor(Math.random() * 6) + 1;
+// document.write(`
+// <p>Random Dice Value: ${diceValue}</p>
+// `);
+
+
+// ************* Question : 7 *****************
+
+// var randomValue = Math.floor(Math.random() * 2);
+
+// var coinResult = (randomValue === 0) ? 'Head' : 'Tails';
+
+// document.write(`
+// <p>Random Coin Value: ${coinResult}</p>
+// `);
+
+
+// ************* Question : 8 *****************
+
+// let randomValue = Math.floor(Math.random() * 100) + 1;
+
+// document.write(`
+// <p>Random Number Between 1 and 100: ${randomValue}</p>
+// `);
+
+
+// ************* Question : 9 *****************
+
