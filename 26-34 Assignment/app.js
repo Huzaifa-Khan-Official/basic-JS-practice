b8d3f21f3fef92b28e4280e79e421bc80427709e
Huzaifa Khan <122217933+Huzaifa-Khan-Official@users.noreply.github.com>
2023-10-06 03:02:38 -0700

    done assignment

--- 26-34 Assignment/app.js
+++ 26-34 Assignment/app.js
@@ -101,3 +101,87 @@
 
 // ************* Question : 9 *****************
 
+// var userInput = prompt("Please enter your weight (e.g., 50, 50kgs, 50.2kgs, 50.2kilograms):");
+
+// var numericValue = parseFloat(userInput.replace(/[^\d.]/g, ""));
+
+// if (!isNaN(numericValue)) {
+//   document.write("<p>Your weight is: " + numericValue + " kilograms</p>");
+// } else {
+//   document.write("<p>Invalid input.</p>");
+// }
+
+
+// ************* Question : 10 *****************
+
+// var randomNumber = Math.floor(Math.random() * 10) + 1;
+// var inputNum = prompt("Please guess a number between 1 to 10: ");
+
+// if (randomNumber == inputNum) {
+//     alert("Congratulations, you have won")
+// } else {
+//     alert("Sorry, you lost.")
+// }
+
+// ************* Question : 11 *****************
+
+// var date = Date();
+// document.write(date)
+
+// ************* Question : 12 *****************
+
+// var currentDate = new Date();
+
+// var monthNames = [
+//   "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
+// ];
+
+// var currentMonthNumber = currentDate.getMonth();
+
+// var currentMonthName = monthNames[currentMonthNumber];
+
+// alert("Current Month: " + currentMonthName);
+
+
+// ************* Question : 13 *****************
+
+// var currentDate = new Date();
+
+// var dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+// var currentDayNumber = currentDate.getDay();
+
+// var currentDayName = dayNames[currentDayNumber];
+
+
+// var currentDayAbbreviation = currentDayName.slice(0, 3);
+
+
+// alert("Today is: " + currentDayAbbreviation);
+
+
+// ************* Question : 14 *****************
+
+// var currentDate = new Date();
+
+// var currentDayNumber = currentDate.getDay();
+
+
+// if (currentDayNumber === 6 || currentDayNumber === 0) {
+//   alert("It's Fun day");
+// } else {
+//   alert("It's a regular day");
+// }
+
+
+// ************* Question : 15 *****************
+
+// var currentDate = new Date();
+
+// var currentDayOfMonth = currentDate.getDate();
+
+// if (currentDayOfMonth < 16) {
+//   alert("First fifteen days of the month");
+// } else {
+//   alert("Last days of the month");
+// }
